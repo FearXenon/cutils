15e53e2d35530dbdd67007316cfb9a3ca7191d41
FearXenon <118049326+FearXenon@users.noreply.github.com>
2026-02-03 17:21:19 +0100

    convert to header-only

--- cregex.h
+++ cregex.h
@@ -16,3 +16,19 @@ char* regex_match(char* regex, char* string, size_t* n);
 char* regex_replace(char* search, char* replace, char* string);
 void regex_build_dfa(char* regex, regex_engine* engine);
 #endif
+#ifdef CREGEX_IMPLEMENTATION
+
+char *regex_match(char *regex, char *string, size_t *n) {
+
+  regex_engine engine = {};
+
+  regex_build_dfa(regex, &engine);
+
+  return 0;
+}
+
+void regex_build_dfa(char *regex, regex_engine *engine) {
+
+}
+
+#endif
